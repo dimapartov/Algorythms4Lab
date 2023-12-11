0d810a973a2fcaa972496a2a80cdd25d67e58d01
dimapartov <mr_kubarev@mail.ru>
2023-12-11 17:42:22 +0300

    Add main methods for queue and heap Main classes

--- src/main/java/max_heap_linked_list/Main.java
+++ src/main/java/max_heap_linked_list/Main.java
@@ -2,6 +2,23 @@ package max_heap_linked_list;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("MaxHeap");
+
+        MaxHeap<Integer> maxHeap = new MaxHeap<>();
+
+        maxHeap.add(3);
+        maxHeap.add(2);
+        maxHeap.add(5);
+        maxHeap.add(7);
+        maxHeap.add(1);
+        maxHeap.add(20);
+        maxHeap.add(24);
+        maxHeap.add(15);
+        maxHeap.add(9);
+        maxHeap.add(22);
+
+        System.out.println("Heap size: " + maxHeap.size());
+
+        System.out.println("Max. element: " + maxHeap.peek());
+
     }
 }
--- src/main/java/priority_queue_array_list/Main.java
+++ src/main/java/priority_queue_array_list/Main.java
@@ -2,6 +2,28 @@ package priority_queue_array_list;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Queue");
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.add(-5);
+        priorityQueue.add(2);
+        priorityQueue.add(3);
+        priorityQueue.add(12);
+        priorityQueue.add(15);
+        priorityQueue.add(21);
+        priorityQueue.add(33);
+        priorityQueue.add(-10);
+        priorityQueue.add(6);
+        priorityQueue.add(18);
+
+        System.out.println("Queue size: " + priorityQueue.size());
+
+        System.out.println("Highest priority: " + priorityQueue.peek());
+
+        System.out.println("Delete elements with highest priority: ");
+        while (priorityQueue.size() > 0) {
+            System.out.println(priorityQueue.poll() + " ");
+        }
+
     }
 }
